5f0701c674bfd9c32bee940d22ba3a4fefda2cc8
mia-niznai <maria.popa.8735@gmail.com>
2023-04-03 18:33:59 +0300

    color changing bar

--- client/src/components/BackgroundColorButton.js
+++ client/src/components/BackgroundColorButton.js
@@ -1,24 +1,3 @@
-// import React, { useState } from "react";
-
-// function BackgroundColorButton(props) {
-//     const { onBgColorChange } = props;
-//     const [bgColor, setBgColor] = useState("#9e9e9e");
-
-//     return (
-//         <button className="background">
-//             Choose background color
-//             <input
-//                 type="color"
-//                 placeholder="Choose background color"
-//                 value={bgColor}
-//                 onChange={(e) => onBgColorChange(e.target.value)}
-//             />
-//         </button>
-//     );
-// }
-
-// export default BackgroundColorButton;
-
 import React, { useState } from "react";
 
 function BackgroundColorButton(props) {
@@ -27,7 +6,7 @@ function BackgroundColorButton(props) {
 
     const handleColorChange = (e) => {
         const color = e.target.value;
-        setBgColor(color); // Update bgColor state
+        setBgColor(color); 
         onBgColorChange(color);
     };
 
