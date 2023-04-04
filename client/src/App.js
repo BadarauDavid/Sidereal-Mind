b05a8560776b8bef72654f22f69cfcf8240c916d
Pinkydvs <pinky_dvs@yahoo.com>
2023-04-04 09:47:08 +0300

    update components

--- client/src/App.js
+++ client/src/App.js
@@ -2,13 +2,7 @@ import './App.css';
 import React, { useState, useEffect } from "react";
 import BackgroundColorButton from './components/BackgroundColorButton';
 import Header from './components/Header';
-import Card1 from './components/Card1';
-import Card2 from './components/Card2';
-import Card3 from './components/Card3';
-import Card4 from './components/Card4';
-import Card5 from './components/Card5';
-import Card6 from './components/Card6';
-import Card7 from './components/Card7';
+import AllCards from './components/AllCards';
 
 
 function App() {
@@ -22,15 +16,7 @@ function App() {
     <div className="App" style={{ backgroundColor: bgColor }}>
       <Header />
       <BackgroundColorButton onBgColorChange={handleBgColor} />
-      <div className='card-container'>
-        <Card1 />
-        <Card2 />
-        <Card3 />
-        <Card4 />
-        <Card5 />
-        <Card6 />
-        <Card7 />
-      </div>
+      <AllCards />
     </div>
 
   );
---
+++ client/src/components/AllCards.js
@@ -0,0 +1,23 @@
+import React, { useState, useEffect } from "react";
+import Card1 from './Card1';
+import Card2 from './Card2';
+import Card3 from './Card3';
+import Card4 from './Card4';
+import Card5 from './Card5';
+import Card6 from './Card6';
+import Card7 from './Card7';
+
+function AllCards () {
+return (
+    <div className='card-container'>
+        <Card1 />
+        <Card2 />
+        <Card3 />
+        <Card4 />
+        <Card5 />
+        <Card6 />
+        <Card7 />
+    </div>
+    )
+}
+export default AllCards;
