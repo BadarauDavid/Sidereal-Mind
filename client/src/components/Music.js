6df86638c826e39b4da26df170b7a94cf8745f1c
mia-niznai <maria.popa.8735@gmail.com>
2023-04-06 17:25:57 +0300

    music

--- client/src/components/Music.js
+++ client/src/components/Music.js
@@ -27,7 +27,15 @@ function Music () {
 
     return (
         <div >
-            <p>Music for programming, stay focused, sleep, relaxation</p>
+        <p>Music for programming, stay focused, sleep, relaxation</p>
+        <br></br>
+        <br></br>
+        <iframe width="560" height="315" src="https://www.youtube.com/embed/0diEVDtCI9o" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+        <br></br>
+        <br></br>
+        <iframe width="560" height="315" src="https://www.youtube.com/embed/WL9EOfzoSsA" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+        <br></br>
+        <br></br>
         </div>
     )
 }
