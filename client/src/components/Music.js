d5432d289ad4f59be9a9293e8870ce1f939bfed7
Pinkydvs <pinky_dvs@yahoo.com>
2023-04-06 15:09:06 +0300

    music fetch

--- client/src/components/Music.js
+++ client/src/components/Music.js
@@ -1,6 +1,30 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 function Music () {
+
+        const API_KEY = "79064c9ee5msh3a853c614f52775p1a84bejsn4e93f7c3af40";
+        const API_URL = "https://deezerdevs-deezer.p.rapidapi.com/search?q=eminem";
+      
+        async function fetchData() {
+          try {
+            const response = await fetch(API_URL, {
+              headers: {
+                "X-RapidAPI-Key": API_KEY,
+                "X-RapidAPI-Host": "deezerdevs-deezer.p.rapidapi.com",
+              },
+            });
+            const music = await response.json();
+            console.log(music);
+          } catch (err) {
+            console.error(err);
+          }
+        }
+    
+    useEffect(() => {
+        fetchData();
+    }, []);
+    
+
     return (
         <div >
             <p>Music for programming, stay focused, sleep, and relaxation</p>
